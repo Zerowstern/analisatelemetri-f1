355aaf57bf342fea632ae9c815500db0ab9b076f
Zerowstern <nghqsqtvzv@privaterelay.appleid.com>
2026-05-01 19:16:25 +0700

    Fix: add basename to BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,10 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* Tambahkan basename di sini agar sesuai dengan nama repo GitHub Anda */}
+      <BrowserRouter basename="/analisatelemetri-f1">
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
